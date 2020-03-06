4e8ecf0584622400e109f59bb6dae3ee56b38a9e
Tuan Tran <TuanTranEngineer@github.com>
2020-03-06 09:54:10 +0700

    update chapter5

---
+++ Chapter5/genetic_algorithm.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+from typing import TypeVar, Generic, List, Tuple, Callable
+from enum import Enum
+from random import choices, random
+from heapq import nlargest
+from statistics import mean
+from chromosome import Chromosome
+
+C = TypeVar('C', bound = "Chromosome")
+
+class GeneticAlgorithm(Generic[C]):
+    SelectionType = Enum("SelectionType", "ROULETTE TOURNAMENT")
+
+    def __init__(self, initial_population: List[C], threshold: float, max_generations: int = 100, mutation_chance: float = 0.01,
+        crossover_chance: float = 0.7, selection_type: SelectionType = SelectionType.TOURNAMENT) -> None:
+        self._population: List[C] = initial_population
+        self._threshold: float = threshold
+        self._max_generations: int = max_generations
+        self._mutation_chance: float = mutation_chance
+        self._crossover_chance: float = crossover_chance
+        self._selection_type: GeneticAlgorithm.SelectionType = selection_type
+        self._fitness_key: Callable = type(self._population[0]).fitness
+
+    def _pick_roulette(self, wheel: List[float]) -> Tuple[C, C]:
+        return tuple(choices(self._population, weights = wheel, k = 2))
+
+    def _pick_tournament(self, num_participants: int) -> Tuple[C, C]:
+        participants: List[C] = choices(self._population, k = num_participants)
+        return tuple(nlargest(2, participants, key = self._fitness_key))
+
+    def _reproduce_and_replace(self) -> None:
+        new_population: List[C] = []
+        while len(new_population) < len(self._population):
+            if self._selection_type == GeneticAlgorithm.SelectionType.ROULETTE:
+                parents: Tuple[C, C] = self._pick_roulette([x.fitness() for x in self._population])
+            else:
+                parents: Tuple[C, C] = self._pick_tournament(len(self._population) // 2)
+            if random() < self._crossover_chance:
+                new_population.extend(parents[0].crossover(parents[1]))
+            else:
+                new_population.extend(parents)
+        if len(new_population) > len(self._population):
+            new_population.pop()
+        self._population = new_population
+
+    def _mutate(self) -> None:
+        for individual in self._population:
+            if random() < self._mutation_chance:
+                individual.mutate()
+
+    def run(self) -> C:
+        best: C = max(self._population, key = self._fitness_key)
+        for generation in range(self._max_generations):
+            if best.fitness() >= self._threshold: return best
+            print(f"Generation {generation} Best {best.fitness()} Avg {mean(map(self._fitness_key, self._population))}")
+            self._reproduce_and_replace()
+            self._mutate()
+            highest: C = max(self._population, key = self._fitness_key)
+            if highest.fitness() > best.fitness(): best = highest
+        return best
---
+++ Chapter5/send_more_money.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+from typing import Tuple, List
+from chromosome import Chromosome
+from genetic_algorithm import GeneticAlgorithm
+from random import shuffle, sample
+from copy import deepcopy
+
+class SendMoreMoney(Chromosome):
+    def __init__(self, letters: List[str]) -> None:
+        self.letters: List[str] = letters
+
+    def fitness(self) -> float:
+        s: int = self.letters.index("S")
+        e: int = self.letters.index("E")
+        n: int = self.letters.index("N")
+        d: int = self.letters.index("D")
+        m: int = self.letters.index("M")
+        o: int = self.letters.index("O")
+        r: int = self.letters.index("R")
+        y: int = self.letters.index("Y")
+        send: int = s*1000 + e*100 + n*10 + d
+        more: int = m*1000 + o*100 + r*10 + e
+        money: int = m*10000 + o*1000 + n*100 + e*10 + y
+        difference: int = abs(money - send - more)
+        return 1/(difference + 1)
+
+    @classmethod
+    def random_instance(cls):
+        letters = ["S", "E", "N", "D", "M", "O", "R", "Y", "", ""]
+        shuffle(letters)
+        return SendMoreMoney(letters)
+
+    def crossover(self, other: SendMoreMoney) -> Tuple[SendMoreMoney, SendMoreMoney]:
+        child1: SendMoreMoney = deepcopy(self)
+        child2: SendMoreMoney = deepcopy(other)
+        idx1, idx2 = sample(range(len(self.letters)), k = 2)
+        l1, l2 = child1.letters[idx1], child2.letters[idx2]
+        child1.letters[child1.letters.index(l2)], child1.letters[idx2] = child1.letters[idx2], l2
+        child2.letters[child1.letters.index(l1)], child2.letters[idx1] = child2.letters[idx1], l1
+        return child1, child2
+
+    def mutate(self) -> None:
+        idx1, idx2 = sample(range(len(self.letters)), k = 2)
+        self.letters[idx1], self.letters[idx2] = self.letters[idx2], self.letters[idx1]
+
+    def __str__(self) -> str:
+        s: int = self.letters.index("S")
+        e: int = self.letters.index("E")
+        n: int = self.letters.index("N")
+        d: int = self.letters.index("D")
+        m: int = self.letters.index("M")
+        o: int = self.letters.index("O")
+        r: int = self.letters.index("R")
+        y: int = self.letters.index("Y")
+        send: int = s*1000 + e*100 + n*10 + d
+        more: int = m*1000 + o*100 + r*10 + e
+        money: int = m*10000 + o*1000 + n*100 + e*10 + y
+        difference: int = abs(money - send - more)
+        return f"{send} + {more} = {money} Difference: {difference}"
+
+if __name__ == "__main__":
+    initial_population: List[SendMoreMoney] = [SendMoreMoney.random_instance() for _ in range(1000)]
+    ga: GeneticAlgorithm[SendMoreMoney] = GeneticAlgorithm(initial_population = initial_population, threshold = 1.0,
+        max_generations = 1000, mutation_chance = 0.2, crossover_chance = 0.7, selection_type = GeneticAlgorithm.SelectionType.ROULETTE)
+    result: SendMoreMoney = ga.run()
+    print(result)

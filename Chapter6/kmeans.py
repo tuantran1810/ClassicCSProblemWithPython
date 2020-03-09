f5ea02a676b3afc8728eba26b9c2fcec54fdc4c7
Tuan Tran <TuanTranEngineer@github.com>
2020-03-09 23:07:35 +0700

    complete chapter6

--- Chapter6/kmeans.py
+++ Chapter6/kmeans.py
@@ -7,7 +7,7 @@ from statistics import mean, pstdev
 from dataclasses import dataclass
 from data_point import DataPoint
 
-def zscore(original: Sequence[float]) -> List[float]:
+def zscores(original: Sequence[float]) -> List[float]:
     avg: float = mean(original)
     std: float = pstdev(original)
     if std == 0: return [0] * len(original)
@@ -42,14 +42,14 @@ class KMeans(Generic[Point]):
         zscored: List[List[float]] = [[] for _ in range(len(self._points))]
         for dimension in range(self._points[0].num_dimensions):
             dimension_slice: List[float] = self._dimension_slice(dimension)
-            for index, zscore in enumerate(zscore(dimension_slice)):
+            for index, zscore in enumerate(zscores(dimension_slice)):
                 zscored[index].append(zscore)
         for i in range(len(self._points)):
             self._points[i].dimensions = tuple(zscored[i])
 
     def _random_point(self) -> DataPoint:
         rand_dimensions: List[float] = []
-        for dimension in range(self._points[0].dimensions):
+        for dimension in range(self._points[0].num_dimensions):
             values: List[float] = self._dimension_slice(dimension)
             rand_value: float = uniform(min(values), max(values))
             rand_dimensions.append(rand_value)
@@ -83,3 +83,11 @@ class KMeans(Generic[Point]):
                 return self._clusters
         return self._clusters
 
+if __name__ == "__main__":
+    point1: DataPoint = DataPoint([2.0, 1.0, 1.0])
+    point2: DataPoint = DataPoint([2.0, 1.0, 5.0])
+    point3: DataPoint = DataPoint([3.0, 1.5, 2.5])
+    kmeans_test: KMeans[DataPoint] = KMeans(2, [point1, point2, point3])
+    test_clusters: List[KMeans.Cluster] = kmeans_test.run()
+    for index, cluster in enumerate(test_clusters):
+        print(f"Cluster {index}: {cluster.points}")

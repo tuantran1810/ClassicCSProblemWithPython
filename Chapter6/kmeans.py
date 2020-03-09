3d0fdadf2df173dc0a9be93614fe80bebb6f724a
Tuan Tran <TuanTranEngineer@github.com>
2020-03-09 09:21:04 +0700

    update for kmeans

--- Chapter6/kmeans.py
+++ Chapter6/kmeans.py
@@ -25,13 +25,15 @@ class KMeans(Generic[Point]):
         if k < 1: raise ValueError("k must be >= 1")
         self._points: List[Point] = points
         self._zscore_normalize()
-        self._cluster: List[KMeans.Cluster] = []
+        self._clusters: List[KMeans.Cluster] = []
         for _ in range(k):
-            rand_point: DataPoint = self._rando
+            rand_point: DataPoint = self._random_point()
+            cluster: KMeans.Cluster = KMeans.Cluster([], rand_point)
+            self._clusters.append(cluster)
 
     @property
     def _centroids(self) -> List[DataPoint]:
-        return [x.centroid for x in self._cluster]
+        return [x.centroid for x in self._clusters]
 
     def _dimension_slice(self, dimension: int) -> List[float]:
         return [x.dimensions[dimension] for x in self._points]
@@ -53,8 +55,31 @@ class KMeans(Generic[Point]):
             rand_dimensions.append(rand_value)
         return DataPoint(rand_dimensions)
 
+    def _assign_clusters(self) -> None:
+        for point in self._points:
+            closest: DataPoint = min(self._centroids, key = partial(DataPoint.distance, point))
+            idx: int = self._centroids.index(closest)
+            cluster: KMeans.Cluster = self._clusters[idx]
+            cluster.points.append(point)
+
     def _generate_centroids(self) -> None:
-        for cluster in self.
+        for cluster in self._clusters:
+            if len(cluster.points) == 0: continue
+            means: List[float] = []
+            for dimension in range(self._points[0].num_dimensions):
+                dimension_slice: List[float] = [x.dimensions[dimension] for x in cluster.points]
+                means.append(mean(dimension_slice))
+            cluster.centroid = DataPoint(means)
 
-
+    def run(self, max_iterations: int = 100) -> List[KMeans.Cluster]:
+        for iteration in range(max_iterations):
+            for cluster in self._clusters:
+                cluster.points.clear()
+            self._assign_clusters()
+            old_centroids: List[DataPoint] = deepcopy(self._centroids)
+            self._generate_centroids()
+            if old_centroids == self._centroids:
+                print(f"Coverged after {iteration} iterations")
+                return self._clusters
+        return self._clusters
 
